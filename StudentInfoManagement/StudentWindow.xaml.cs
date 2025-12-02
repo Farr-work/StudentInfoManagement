de983774a337bce2f9cea872f82df7a897865d3b
Duong dz <farrwork4399@gmail.com>
2025-12-02 16:46:26 +0700

    Khoi phuc lai code duoi may local

---
+++ StudentInfoManagement/StudentWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using StudentInfoManagement.Views;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace StudentInfoManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StudentWindow : Window
+    {
+        public StudentWindow()
+        {
+            InitializeComponent();
+
+            NavigateTo("StudentDashboard");
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag != null)
+            {
+                string viewName = button.Tag.ToString();
+
+                NavigateTo(viewName);
+            }
+        }
+
+        private void NavigateTo(string viewName)
+        {
+            switch (viewName)
+            {
+                case "StudentDashboard":
+                    MainContent.Content = new StudentDashboardView();
+                    // Tạm thời dùng TextBlock để test nếu bạn chưa tạo UserControl
+                    //MainContent.Content = CreatePlaceholder("Dashboard View");
+                    break;
+
+                case "StudentCourses":
+                    MainContent.Content = new CoursesView();
+                    break;
+
+                case "StudentClasses":
+                    MainContent.Content = CreatePlaceholder("Student Portal View");
+                    break;
+
+                case "Settings":
+                    MainContent.Content = new SettingViews();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private TextBlock CreatePlaceholder(string text)
+        {
+            return new TextBlock
+            {
+                Text = text,
+                FontSize = 24,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Foreground = Brushes.Gray
+            };
+        }
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Bạn muốn đăng xuất?", "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
